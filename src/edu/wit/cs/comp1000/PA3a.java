74fc6afa2204515652c87c21a1941e6f7f6f92b4
Jones Yu <yuj6@wit.edu>
2020-10-01 21:09:43 -0400

    Update PA3 to make it generic.

--- src/edu/wit/cs/comp1000/PA3a.java
+++ src/edu/wit/cs/comp1000/PA3a.java
@@ -4,22 +4,19 @@ package edu.wit.cs.comp1000;
 *	Lab 4 - PA3a
 *	YOUR NAME HERE
 *
-*	COMP1000-XX  (ENTER YOUR SESSION, EITHER 01/02 or 03/04)
-*	9/27/2019  (UPDATE THE DATE) 
+*	COMP1000-XX  (XX is your session number)
+*	MM/DD/YYYY  (UPDATE THE DATE) 
 **********************************************************************
-*	Problem Description
+*	Problem Description (UPDATE THE DESCRIPTION)
 *
 *	Write a program that reads two numbers from the user and
-*	prints out the sum of those two numbers. (UPDATE THE DESCRIPTION)
-*
-***********************************************************************
-*	Analysis
+*	prints out the sum of those two numbers. 
+************************************************************************
+*	Analysis (UPDATE INPUTS and OUTPUTS)
 *
 *	Inputs:  Two integers, read from the user.
-*
 *	Outputs: The sum of the inputs.
-*
-**********************************************************************/
+***********************************************************************/
 
 public class PA3a {
 
